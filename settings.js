03c10ce41ca89bcff0259dc79f101ea561c43170
saadf68261 <122790690+saadf68261@users.noreply.github.com>
2023-01-26 17:07:00 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,22 +14,22 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94786802371'] //ur owner number
-global.ownername = "Nimesh Official" //ur owner name
-global.ytname = "YT: HELA CLOUD" //ur yt chanel name
-global.socialm = "GitHub: nimaofficial" //ur github or insta name
-global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
+global.owner = ['923314124309'] //ur owner number
+global.ownername = "Saad Official" //ur owner name
+global.ytname = "YT: ITZ SAAD EDITZ" //ur yt chanel name
+global.socialm = "GitHub: saadf68261" //ur github or insta name
+global.location = "pakistan" //ur location
 
 //bot bomdy 
-global.ownernomer = "94786802371" //ur number
-global.premium = ['94786802371'] //ur premium number
+global.ownernomer = "923314124309" //ur number
+global.premium = ['923314124309'] //ur premium number
 global.botname = 'BLACK DRAGON' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
+global.linkz = "https://chat.whatsapp.com/HYj9yeQbHoS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://www.helacloud.ga" //ur website to be displayed
-global.botscript = 'https://github.com/nimaofficial/Black-Dragon' //script link
+global.botscript = 'https:/ndjdbdbdvd' //script link
 global.themeemoji = "❤️" //ur theme emoji
 global.packname = "Sticker By Black Dragon" //ur sticker watermark packname
-global.author = "Nimesh Official" //ur sticker watermark author
+global.author = "SAAD Official" //ur sticker watermark author
 global.wm = "𝗕𝗟𝗔𝗖𝗞 𝗗𝗥𝗔𝗚𝗢𝗡 𝗜𝗡𝗖" //ur watermark
 
 // Other
